0df8874d8ddfcfe493ab12cadb18f7291dedce37
vk3dan <34954022+vk3dan@users.noreply.github.com>
2020-04-13 16:16:53 +1000

    Add files via upload

--- reddityouranytone.py
+++ reddityouranytone.py
@@ -3,31 +3,48 @@
 import csv
 import urllib2
 import os.path
+import time
 import pandas as pd
 
 dbfile = "user.csv"
 redditfile = "nicks.csv"
-workingfile = "working.csv"
 outputfile = "output.csv"
+current_time = time.time()
+dburl = "https://www.radioid.net/static/user.csv"
+redditurl = "https://raw.githubusercontent.com/molo1134/qrmbot/master/lib/nicks.csv"
 
 print ("\nRedditYourAnytone: A tool for downloading the current DMR database and reddit ham list\nand combining for upload to Anytone DMR HTs by vk3dan\n")
 
 # get the current database csv from radioid.net
 if os.path.isfile(dbfile):
-    print("user.csv found, moving on")
+    print("user.csv found")
+    creation_time = os.path.getctime(dbfile)
+    if (current_time - creation_time) // (24 * 3600) >= 7:
+        print("file over 7 days old, fetching current version (~9MB)")
+        os.unlink(dbfile)
+        response = urllib2.urlopen(dburl)
+        with open(dbfile, 'w') as f: f.write(response.read ())
+    else:
+        print("user.csv current")
 else:
     print("fetching DMR database file")
-    dburl = "https://www.radioid.net/static/user.csv"
     response = urllib2.urlopen(dburl)
-    with open(dbfile, 'w') as f: f.write (response.read ())
+    with open(dbfile, 'w') as f: f.write(response.read ())
 
 # get the current reddit hams csv from molo1134's github
 
 if os.path.isfile(redditfile):
-    print("nicks.csv found, moving on\n")
+    print("nicks.csv found")
+    creation_time = os.path.getctime(redditfile)
+    if (current_time - creation_time) // (24 * 3600) >= 28:
+        print("file over 28 days old, fetching current version (<1MB)\n")
+        os.unlink(redditfile)
+        response = urllib2.urlopen(redditurl)
+        with open(redditfile, 'w') as f: f.write(response.read ())
+    else:
+        print("nicks.csv current\n")
 else:
     print("fetching reddit hams csv file\n")
-    redditurl = "https://raw.githubusercontent.com/molo1134/qrmbot/master/lib/nicks.csv"
     response = urllib2.urlopen(redditurl)
     with open(redditfile, 'w') as f: f.write(response.read())
 
@@ -41,18 +58,12 @@ df.insert(10,"Call Alert","None")
 df.drop(["LAST_NAME", "REMARKS"], axis=1, inplace=True)
 df.insert(7,"Remarks","")
 df = df.rename(columns={"RADIO_ID": "Radio ID", "CALLSIGN": "Callsign", "FIRST_NAME": "Name", "CITY": "City", "STATE": "State", "COUNTRY": "Country"})
-df.to_csv(workingfile, index=False)
-print (repr(df.index.size)+ " DMR IDs output to working.csv")
 
-# reopen working file and reddit file
-wf = open(workingfile, 'r')
-csv_wf = csv.reader(wf)
+# do the reddit limbo
 rf = open(redditfile, 'r')
 csv_rf = csv.reader(rf)
-for row in csv_wf:
-    datawf=[row[2],row[4],row[5]]
-    for row in csv_rf:
-        if row[0]==datawf[0]:
-            print(row[0]) # this doesn't work
-# i don't know wtf to do from here to match callsigns in dmr id to the ones in nicks.csv
-# still to do: the above, and copy irc nicks over City if applicable, and reddit u/names over state, if applicable.
+print ("finding redditors and updating info, this may take a while\n")
+for row in csv_rf:
+    df.loc[df["Callsign"] == row[0], ["City", "State"]] = row[1], row[2]
+df.to_csv(outputfile, index=False)
+print("output.csv created with "+repr(df.index.size)+" entries")
